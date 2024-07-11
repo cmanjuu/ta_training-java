6a4988e6d63774afdbc6c1ad33687c579715a840
Manjula_Chakravaram <manjula_chakravaram@epam.com>
2024-07-11 17:46:05 +0530

    Task1 Done

--- src/test/java/com/epam/manjula_chakravakam/optional_task1/Task1.java
+++ src/test/java/com/epam/manjula_chakravakam/optional_task1/Task1.java
@@ -1,4 +1,19 @@
 package com.epam.manjula_chakravakam.optional_task1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task1 {
+    public static void main(String[] args) {
+
+        WebDriver webDriver=new ChromeDriver();
+        webDriver.get("https://pastebin.com/");
+        webDriver.manage().window().maximize();
+        webDriver.findElement(By.id("postform-text")).sendKeys("Hello from WebDriver");
+        webDriver.findElement(By.id("select2-postform-expiration-container")).click();
+        webDriver.findElement(By.xpath("//li[text()='10 Minutes']")).click();
+        webDriver.findElement(By.id("postform-name")).sendKeys("helloWeb");
+        webDriver.findElement(By.cssSelector("button[type='submit']")).click();
+    }
 }
